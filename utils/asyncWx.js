339c013c22a2d60abf4be84d7801e6bfdd32511c
charlenelbb <56347097+charlenelbb@users.noreply.github.com>
2020-04-23 18:29:19 +0800

    Add files via upload

---
+++ utils/asyncWx.js
@@ -0,0 +1,94 @@
+//promise形式getSetting
+export const getSetting=()=>{
+    return new Promise((resolve,reject)=>{
+        wx.getSetting({
+            success: (result)=>{
+                resolve(result)
+            },
+            fail: (err)=>{reject(err)},
+        });
+    })
+}
+//promise形式chooseAddress
+export const chooseAddress=()=>{
+    return new Promise((resolve,reject)=>{
+        wx.chooseAddress({
+            success: (result)=>{
+                resolve(result)
+            },
+            fail: (err)=>{reject(err)},
+        });
+    })
+}
+//promise形式openSetting
+export const openSetting=()=>{
+    return new Promise((resolve,reject)=>{
+        wx.openSetting({
+            success: (result)=>{
+                resolve(result)
+            },
+            fail: (err)=>{reject(err)},
+        });
+    })
+}
+//promise形式showModal
+export const showModal=({content})=>{
+    return new Promise((resolve,reject)=>{
+        wx.showModal({
+            title: '提示',
+            content: content,
+            success: (result) => {
+                resolve(result);
+            },
+            fail:(err)=>{
+                reject(err);
+            }
+          });
+      })
+}
+
+//promise形式showToast
+export const showToast=({title})=>{
+    return new Promise((resolve,reject)=>{
+        wx.showToast({
+            title: title,
+            icon:'none',
+            success: (result) => {
+                resolve(result);
+            },
+            fail:(err)=>{
+                reject(err);
+            }
+          });
+      })
+}
+
+//promise形式login
+export const login=()=>{
+    return new Promise((resolve,reject)=>{
+        wx.login({
+            timeout:10000,
+            success: (result)=>{
+                resolve(result);
+            },
+            fail:(err)=>{
+                reject(err);
+            }
+          });
+            })
+}
+
+//promise形式login
+export const requestPayment=(pay)=>{
+    return new Promise((resolve,reject)=>{
+        wx.requestPayment({
+            ...pay,
+            success: (result)=>{
+                resolve(result)
+            },
+            fail: (err)=>{
+                reject(err);
+            },
+        });
+            })
+}
